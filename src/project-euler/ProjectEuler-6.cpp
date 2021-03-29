3b51f25dea889732cfa72e458b73079d20abe890
abnercorrea <abnercorreajr@gmail.com>
2021-03-29 07:20:48 -0300

    Cleaning up Project Euler problem 6

--- src/project-euler/ProjectEuler-6.cpp
+++ src/project-euler/ProjectEuler-6.cpp
@@ -3,29 +3,6 @@
 
 using namespace std;
 
-// PROBLEMATIC, unsigned long overflow
-// sumSquareDifferenceULong(49797) =     71023086286612
-// sumSquareDifference(49797) = 1537299695895415808
-unsigned long powul(unsigned long base, int exponent) {
-    unsigned long n = 1UL;
-    for (int i = 0; i < exponent; i++) n *= base;
-    return n;
-}
-
-// PROBLEMATIC, unsigned long overflow
-// sumSquareDifferenceULong(49797) =     71023086286612
-// sumSquareDifference(49797) = 1537299695895415808
-unsigned long sumSquareDifferenceULong(unsigned long n) {
-    return (3 * powul(n, 4) + 2 * powul(n, 3) - 3 * powul(n, 2) - 2 * n) / 12;
-}
-
-// PROBLEMATIC, narrowing unsigned long -> double conversion!
-// sumSquareDifference(9745) =     2254743208597880
-// sumSquareDifferencePow(9745) =  2254743208597879
-unsigned long sumSquareDifferencePow(unsigned long n) {
-    return (3 * pow(n, 4) + 2 * pow(n, 3) - 3 * pow(n, 2) - 2 * n) / 12;
-}
-
 /**
  * 1. Sum of the first n integers:
  * Sn = (1 + n) * n / 2
@@ -59,9 +36,6 @@ unsigned long sumSquareDifference(unsigned long n) {
 int main() {
     cout << ULONG_MAX << endl;
     cout << MAXFLOAT << endl;
-
-    cout << "sumSquareDifferencePow 9744: " << sumSquareDifferencePow(9744) << " - 9745: " << sumSquareDifferencePow(9745) << endl;
-    cout << "sumSquareDifferenceULong 49796: " << sumSquareDifferenceULong(49796) << " - 49797: " << sumSquareDifferenceULong(49797) << endl;
     cout << "sumSquareDifference 92681: " << sumSquareDifference(92681) << " - 92682: " << sumSquareDifference(92682) << endl;
 
     cout << sumSquareDifference(100) << endl;
